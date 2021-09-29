7ff3638546fd1dacb341fb4eafa2546e5b2b111e
Luna-Klatzer <luna.klatzer@gmail.com>
2021-09-29 23:02:58 +0200

    Updated index.ts file and attempted to fix tests (unsuccessfully)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('This is the start of this project');
+/*
+ * Main Entry file for the project
+ */
+
+function run() {
+  console.log("This is the start of this project - Kipper v0.0.1");
+}
+
+export { run as kipperRunCLI };
