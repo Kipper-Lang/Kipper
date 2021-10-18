1d894c2cbd71728631dedfb070d124d5a2dd5118
Luna-Klatzer <luna.klatzer@gmail.com>
2021-10-18 12:42:49 +0200

    Added compiler.ts

---
+++ src/compiler/compiler.ts
@@ -0,0 +1,13 @@
+// Main Compiler file for interacting with the entire Kipper Compiler
+
+export class KipperCompiler {
+
+    async compile(file: string, encoding: string)
+    {
+        // TODO! Implement parsing function - add to this function
+        //  Add Visitor class and implementation for generating logic stream (lexing and parsing)
+        //  Compile C-tokens using the logic stream (compilation)
+        //  Validate the existence of all items (linker)
+        //  Generate Code (code generator)
+    }
+}
