043c733ef67a5bac9eb9649c6041fbaeae4367bd
Luna-Klatzer <luna.klatzer@gmail.com>
2021-10-18 12:49:07 +0200

    Added docstrings and parse() function as base - will have to implement antlr4 later

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -2,12 +2,27 @@
 
 export class KipperCompiler {
 
-    async compile(file: string, encoding: string)
+    /**
+     * Parses a file and generates the antlr4 tree, using the function .compilationUnit (parent item of the tree)
+     */
+    async parse(): Promise<any>
+    {
+
+    }
+
+    /**
+     * Compiles a file and generate
+     * @param file The entry-file of the program
+     * @param encoding The encoding that should be used for the program
+     */
+    async compile(file: string, encoding: string): Promise<string>
     {
         // TODO! Implement parsing function - add to this function
         //  Add Visitor class and implementation for generating logic stream (lexing and parsing)
         //  Compile C-tokens using the logic stream (compilation)
         //  Validate the existence of all items (linker)
         //  Generate Code (code generator)
+
+        return "";
     }
 }
