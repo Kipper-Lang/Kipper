047fdaffdf7df56dc19a9aeee02d4b5091acfc4b
Luna-Klatzer <luna.klatzer@gmail.com>
2021-11-23 08:32:30 +0100

    Removed version output in hello.ts

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,17 +1,16 @@
 // Simple hello-command - originated as base command to include content for the pre-publish
 import { Command } from "@oclif/command";
-import { version } from "..";
 
 export default class Hello extends Command {
   static description = "Returns a simple hello from Kipper";
 
   static examples = [
     `$ kipper hello
-Hello from Kipper v0.0.2!
+Hello from Kipper!
 `,
   ];
 
   async run() {
-    this.log(`Hello from Kipper v${version}!`);
+    this.log(`Hello from Kipper!`);
   }
 }
