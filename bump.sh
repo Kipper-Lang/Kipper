0c93ebdbc2ace68acae5bfdabf7e2e8696198574
luna <luna.klatzer@gmail.com>
2022-05-09 20:19:50 +0200

    Updated printf format in bump.sh

--- bump.sh
+++ bump.sh
@@ -15,28 +15,28 @@ else
   fi
 
   # Run the version command for the root package
-  printf "\n -- Updating root project:\n"
+  printf "\n -- Updating root project\n"
   pnpm version "$1"
 
   # Revert the auto-generated commit
-  printf "\n -- Removed generated commit from pnpm version:\n"
+  printf "\n -- Removed generated commit from pnpm version\n"
   git reset --soft HEAD~1
 
   # Delete the auto generated tag
-  printf "\n -- Remove generated from pnpm version:\n"
+  printf "\n -- Remove generated from pnpm version\n"
   git tag -d "v$1"
 
   # Run the version command for every package
-  printf "\n -- Updating child projects:\n"
+  printf "\n -- Updating child projects\n"
   pnpm -r version "$1"
 
   # Commit changes
-  printf "\n -- Committing changes:\n"
-  git commit -a -m "Bumped package versions to \"$1\""
+  printf "\n -- Committing changes\n"
+  git commit -a -m "Release $1"
   git tag -a "v$1" -m "Release $1"
 
   # Update lock files
-  printf "\n -- Updating lock files:\n"
+  printf "\n -- Updating lock files\n"
   pnpm install
 
   # Push tags
