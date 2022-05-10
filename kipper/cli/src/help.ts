928e210d66f19878169903c6dd90402afed07755
luna <luna.klatzer@gmail.com>
2022-05-10 09:22:57 +0200

    Overwrote default help command

---
+++ kipper/cli/src/help.ts
@@ -0,0 +1,12 @@
+/**
+ * 'help' command for displaying help for the CLI. This implements the base help class which generates the help
+ * interface.
+ * @author Luna Klatzer
+ * @copyright 2021-2022 Luna Klatzer
+ * @since 0.5.0
+ */
+import {Help as OclifHelp} from "@oclif/help";
+
+export default class Help extends OclifHelp {
+  static description = "Displays help for the Kipper CLI";
+}
