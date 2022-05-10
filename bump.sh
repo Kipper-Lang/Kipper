fcafbe7033d9b7fca212e6204f34728acf200d80
LunaK <luna.klatzer@gmail.com>
2022-05-10 14:59:11 +0200

    Added `git fetch --all --tags` to bump.sh to avoid overwrites of pre-defined tags

--- bump.sh
+++ bump.sh
@@ -5,38 +5,44 @@ set -e
 
 # Check whether there is a version argument
 if [ -z "$1" ]; then
-  printf "No version identifier supplied\n"
+  printf "ERR: No version identifier supplied!"
   exit 1
 else
+    printf "-- Bumping version %s" "$1"
+
+    # Fetching tags to make sure unknown tags are not re-created
+    printf "-- Fetching tags and releases from remote"
+    git fetch --all --tags
+
   # shellcheck disable=SC2046
   if [ $(git tag -l "v$1") ]; then
-    printf "ERR: Git tag v%s already exists\n" "$1"
+    printf "ERR: Git tag v%s already exists!" "$1"
     exit 1
   fi
 
   # Run the version command for the root package
-  printf "\n -- Updating root project\n"
+  printf "-- Updating root project"
   pnpm version "$1"
 
   # Revert the auto-generated commit
-  printf "\n -- Removed generated commit from pnpm version\n"
+  printf "-- Removed generated commit from pnpm version"
   git reset --soft HEAD~1
 
   # Delete the auto generated tag
-  printf "\n -- Remove generated from pnpm version\n"
+  printf "-- Remove generated tag from pnpm version"
   git tag -d "v$1"
 
   # Run the version command for every package
-  printf "\n -- Updating child projects\n"
+  printf "-- Updating child projects"
   pnpm -r version "$1"
 
   # Commit changes
-  printf "\n -- Committing changes\n"
+  printf "-- Committing changes"
   git commit -a -m "Release $1"
   git tag -a "v$1" -m "Release $1"
 
   # Update lock files
-  printf "\n -- Updating lock files\n"
+  printf "-- Updating lock files"
   pnpm install
 
   # Push tags
