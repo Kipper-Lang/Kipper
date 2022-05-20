f94aaf1ca47b8391e1a9fe09c803a9dedae5c25c
Luna-Klatzer <luna.klatzer@gmail.com>
2022-05-20 23:51:18 +0200

    Removed exception catching and assertions in analyse test, as runtime compiler errors won't be returned directly but only logged

--- test/module/cli/analyse.test.ts
+++ test/module/cli/analyse.test.ts
@@ -23,20 +23,6 @@ describe("analyse", () => {
 			.stderr()
 			.stdout()
 			.command(["analyse", invalidFilePath])
-			.catch((err) => {
-				expect(err instanceof KipperSyntaxError, "Expected proper syntax error");
-
-				// Every data field should be filled
-				expect((<KipperSyntaxError<any>>err).stack).to.not.be.undefined;
-				expect((<KipperSyntaxError<any>>err).line).to.not.be.undefined;
-				expect((<KipperSyntaxError<any>>err).col).to.not.be.undefined;
-				expect((<KipperSyntaxError<any>>err).filePath).to.not.be.undefined;
-				expect((<KipperSyntaxError<any>>err).tokenSrc).to.not.be.undefined;
-				expect((<KipperSyntaxError<any>>err).getTraceback().includes("Unknown")).to.be.false;
-
-				// The path should be passed down to the error
-				expect((<KipperSyntaxError<any>>err).filePath).to.equal(invalidFilePath, "Paths should match");
-			})
 			.it("Invalid syntax", (ctx) => {
 				expect(ctx.stdout).to.length.greaterThan(0);
 				expect(ctx.stdout).to.contain("Starting syntax check for 'invalid.kip'.");
