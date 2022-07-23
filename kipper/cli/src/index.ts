8d3a9339eb2a59ec546d21a1fcc0b4df0a66c3f7
Luna-Klatzer <luna.klatzer@gmail.com>
2022-07-23 17:20:40 +0200

    Bumped static index.ts versions to 0.9.2

--- kipper/cli/src/index.ts
+++ kipper/cli/src/index.ts
@@ -14,7 +14,7 @@ export * as help from "./help";
 // eslint-disable-next-line no-unused-vars
 export const name = "@kipper/cli";
 // eslint-disable-next-line no-unused-vars
-export const version = "0.9.1";
+export const version = "0.9.2";
 // eslint-disable-next-line no-unused-vars
 export const author = "Luna Klatzer";
 // eslint-disable-next-line no-unused-vars
