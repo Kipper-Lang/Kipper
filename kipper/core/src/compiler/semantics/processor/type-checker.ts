3cefc7f82f4df01c71f68c28e5f13d85d93e8f2d
Luna-Klatzer <luna.klatzer@gmail.com>
2022-10-02 17:11:46 +0200

    Fixed bug for return statements with empty value not being type-checked

--- kipper/core/src/compiler/semantics/processor/type-checker.ts
+++ kipper/core/src/compiler/semantics/processor/type-checker.ts
@@ -286,9 +286,12 @@ export class KipperTypeChecker extends KipperSemanticsAsserter {
 	 * @since 0.10.0
 	 */
 	public validReturnStatement(returnStatement: ReturnStatement): void {
-		const returnValueType = returnStatement.getSemanticData().returnValue?.getTypeSemanticData().evaluatedType;
+		// If the return statement has no return value, then the value is automatically 'void'
+		const returnValueType = returnStatement.getSemanticData().returnValue?.getTypeSemanticData().evaluatedType ?? "void";
 		const functionSemanticData = returnStatement.getSemanticData().function.getSemanticData();
 
+		// We need to check whether the types are matching, but *not* if the function return type is valid, since that
+		// will be done later by the function itself during the type checking.
 		if (returnValueType && !this.checkMatchingTypes(returnValueType, <KipperType>functionSemanticData.returnType)) {
 			throw this.assertError(
 				new TypeError(`Type '${returnValueType}' is not assignable to type '${functionSemanticData.returnType}'.`),
