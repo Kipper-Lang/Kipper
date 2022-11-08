6d33220dbfd51153c2901108eb08657a2eeb20f6
luna <luna.klatzer@gmail.com>
2022-11-08 13:35:51 +0100

    Fixed minor bug for markdown content in changelog.ejs

--- src/js/markdown.ts
+++ src/js/markdown.ts
@@ -4,11 +4,15 @@
 
 // Ensure that the anchors of the headers are clickable.
 document.querySelector("body").addEventListener("click", (e) => {
+  // The markdown content body (parent of all markdown elements)
+  const markdownContentBody = document.getElementById("markdown-content");
+
 	// If the target is a heading and has a unique id set, then we can navigate to it.
 	if (e.target instanceof HTMLHeadingElement && e.target.id.length > 0) {
-		// Ensure that the element is a heading inside a Markdown section
-		const mdHeading = document.querySelector(`#markdown-content #${e.target.id}`);
-		if (mdHeading === e.target) {
+		const mdHeading = document.getElementById(e.target.id);
+
+    // Ensure that the element is a heading inside a markdown content body
+		if (mdHeading === e.target && markdownContentBody.contains(mdHeading)) {
 			// Add the anchor to the URL
 			window.location.hash = e.target.id;
 
