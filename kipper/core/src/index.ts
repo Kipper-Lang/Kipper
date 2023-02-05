89bec3f4ebb5f1a2ee01afdfdd793041af22a3a1
Luna-Klatzer <luna.klatzer@gmail.com>
2023-02-05 14:12:58 +0100

    Changed license identifiers in project from deprecated "GPL-3.0" to new SPDX-3.0 "GPL-3.0-or-later" (View https://spdx.org/licenses/GPL-3.0-or-later.html for info)
    Signed-off-by: Luna-Klatzer <luna.klatzer@gmail.com>

--- kipper/core/src/index.ts
+++ kipper/core/src/index.ts
@@ -20,6 +20,6 @@ export const version = "0.10.0-alpha.6";
 // eslint-disable-next-line no-unused-vars
 export const author = "Luna Klatzer";
 // eslint-disable-next-line no-unused-vars
-export const license = "GPL-3.0";
+export const license = "GPL-3.0-or-later";
 // eslint-disable-next-line no-unused-vars
 export const github = "https://github.com/Luna-Klatzer/Kipper";
