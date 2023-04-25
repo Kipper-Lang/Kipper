c6b82a1e99a546efca5741b995106ac41a8d8e4f
luna <luna.klatzer@gmail.com>
2023-04-25 09:57:36 +0200

    Updated phone-optimised menu design and improved layout

---
+++ src/js/header-nav.ts
@@ -0,0 +1,28 @@
+/**
+ * Main for the navigation bar for all pages on the website.
+ */
+
+const phoneHeaderMenu = <HTMLOListElement>document.querySelector("ul#phone-header-nav-bar-menu");
+const phoneHeaderMenuToggle = <HTMLLIElement>document.querySelector(".phone-header-nav-bar-menu-toggle");
+
+/**
+ * Enables the visibility of the dropdown menu.
+ *
+ * Only works on phones.
+ */
+function togglePhoneHeaderMenu() {
+  console.log(phoneHeaderMenu);
+  const isVisible = window.getComputedStyle(phoneHeaderMenu).visibility === "visible";
+  console.log(isVisible);
+  if (isVisible) {
+    phoneHeaderMenu.classList.add("invisible");
+    phoneHeaderMenu.classList.remove("visible");
+  } else {
+    phoneHeaderMenu.classList.add("visible");
+    phoneHeaderMenu.classList.remove("invisible");
+  }
+}
+
+phoneHeaderMenuToggle.addEventListener("click", togglePhoneHeaderMenu);
+console.log(phoneHeaderMenuToggle);
+
--- src/js/search.ts
+++ src/js/search.ts
@@ -4,20 +4,19 @@
 import { PageIndex, searchIndex } from "./search-index";
 import { isDocsFile } from "./main";
 
+const searchOverlay = <HTMLDivElement>document.getElementById("search-result-overlay");
+const searchResult = <HTMLDivElement>document.getElementById("search-result");
+
 export function capitalizeFirstLetter(string: string): string {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function setSearchVisibility(visible: boolean): void {
-	document.getElementById("search-result").style.visibility = visible ? "visible" : "hidden";
-	document.getElementById("search-result-overlay").style.visibility = visible ? "visible" : "hidden";
+  searchResult.style.visibility = visible ? "visible" : "hidden";
+  searchOverlay.style.visibility = visible ? "visible" : "hidden";
 }
 
-export async function search(): Promise<void> {
-	const overlaySearch = <HTMLDivElement>document.getElementById("search-result-overlay");
-	const searchResult = <HTMLDivElement>document.getElementById("search-result");
-	const searchBarInput = <HTMLInputElement>document.getElementById("search-bar-input");
-	const searchBar = <HTMLLIElement>document.getElementById("search-bar");
+export async function search(searchBar, searchBarInput): Promise<void> {
 
 	// Initialise basic interface
 	setSearchVisibility(true);
@@ -64,23 +63,27 @@ export async function search(): Promise<void> {
 
 	// Set position of the search result box
 	const searchBarRect: DOMRect = searchBar.parentElement.getBoundingClientRect();
-	overlaySearch.style.left = `${searchBarRect.left}px`;
-	overlaySearch.style.top = `${searchBarRect.top + searchBarRect.height}px`;
-	overlaySearch.style.width = `${searchBarRect.width}px`;
+	searchOverlay.style.left = `${searchBarRect.left}px`;
+	searchOverlay.style.top = `${searchBarRect.top + searchBarRect.height}px`;
+	searchOverlay.style.width = `${searchBarRect.width}px`;
 }
 
 export async function loadSearch(): Promise<void> {
 	// Verify that the index is complete
-	if (searchIndex === []) {
-		console.error("Critical Error: Index for the search failed to load! Searching is disabled.");
-		document.getElementById("search-bar").style.visibility = "hidden";
+	if (searchIndex !== []) {
+    console.log("Index initialised!");
 	} else {
-		console.log("Index initialised!");
+    console.error("Critical Error: Index for the search failed to load! Searching is disabled.");
+    document.getElementById("search-bar").style.visibility = "hidden";
 	}
 
 	// Add event listener
-	document.getElementById("search-bar-input").addEventListener("keyup", search);
-	document.getElementById("search-bar-input").addEventListener("focus", search);
+	document.querySelectorAll(".search-bar").forEach((searchBar: HTMLLIElement) => {
+    const searchBarInput = <HTMLInputElement>searchBar.children.item(1);
+
+    searchBar.addEventListener("keyup", () => search(searchBar, searchBarInput));
+    searchBar.addEventListener("focus", () => search(searchBar, searchBarInput));
+  });
 	console.log("Search loaded!");
 }
 
@@ -89,10 +92,13 @@ window.addEventListener("DOMContentLoaded", loadSearch);
 
 // If the user clicks outside the search, remove search
 document.addEventListener("click", (e: MouseEvent) => {
-	if (
-		!document.getElementById("search-result-overlay").contains(<Element>e.target) &&
-		!document.getElementById("search-bar").contains(<Element>e.target)
-	) {
+  const selectedInOverlay = document.getElementById("search-result-overlay").contains(<Element>e.target);
+  const selectedInSearchBar = Array.from(
+    document.querySelectorAll(".search-bar")
+  ).find((el) => el.contains(<Element>e.target));
+
+  // Disable visibility if the checks fail
+	if (!selectedInOverlay && !selectedInSearchBar) {
 		setSearchVisibility(false);
 	}
 });
