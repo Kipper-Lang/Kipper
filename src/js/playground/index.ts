5e4d3caa81cfcdb51e0253ea49d5b85764c7837d
luna <luna.klatzer@gmail.com>
2023-06-25 16:36:55 +0200

    Finished remaining bugs in the mobile edition of the playground

--- src/js/playground/index.ts
+++ src/js/playground/index.ts
@@ -3,12 +3,13 @@
  */
 
 // Editor elements
+const interactiveCodeEditor: HTMLDivElement = document.querySelector("#interactive-code-editor");
+const playgroundOptionMenu: HTMLDivElement = document.querySelector("#playground-option-menu");
 const codeEditor: HTMLDivElement = document.querySelector("#code-editor");
 const codeTextArea: HTMLTextAreaElement = document.querySelector("#code-editor-textarea");
 const codeTextAreaHighlightField: HTMLElement = document.querySelector("#code-editor-highlighting-field");
 
 // Sidebar editor fields
-const shellSidebar: HTMLDivElement = document.querySelector("#shell-sidebar");
 const shellOutput: HTMLDivElement = document.querySelector("#shell-output");
 const shellOutputResult: HTMLElement = document.querySelector("#shell-sidebar-highlight-field-content");
 
@@ -27,14 +28,20 @@ function setEditorAndConsoleSizes(): void {
 
   if (window.innerWidth <= mediumScreenSize) {
     // -- Small screen configuration (Automatically done by CSS) --
-    codeTextAreaHighlightField.style.height = `unset`;
-    codeTextAreaHighlightField.style.width = `unset`;
-    codeTextArea.style.height = `unset`;
-    codeTextArea.style.width = `unset`;
-    shellSidebar.style.height = `unset`;
-    shellSidebar.style.width = `unset`;
-    shellOutputResult.style.height = `unset`;
-    shellOutputResult.style.width = `unset`;
+    // Set editor size
+    // - Subtract 2rem for the height due to an inner padding of 1rem
+    // - Unset the width as it will automatically fill up the available space
+    codeEditor.style.height = `${interactiveCodeEditor.clientHeight - playgroundOptionMenu.clientHeight}px`;
+
+    codeTextAreaHighlightField.style.height = `${codeEditor.clientHeight - 2 * rem}px`;
+    codeTextAreaHighlightField.style.width = undefined;
+    codeTextArea.style.height = `${codeEditor.clientHeight - 2 * rem}px`;
+    codeTextArea.style.width = undefined;
+
+    // Set console size
+    // - Subtract 2rem for the height due to an inner padding of 1rem
+    shellOutputResult.style.height = `${shellOutput.clientHeight - 2 * rem}px}`;
+    shellOutputResult.style.width = undefined;
   } else {
     // -- Large screen configuration --
     // Set editor size
@@ -45,10 +52,18 @@ function setEditorAndConsoleSizes(): void {
     codeTextArea.style.height = `${codeEditor.clientHeight - 2 * rem}px`;
     codeTextArea.style.width = `${codeEditor.clientWidth - 4 * rem}px`;
 
-    // Set console size. Subtracts -2rem due to an inner padding of 1rem
+    // Set console size
+    // - Subtracts 2rem due to an inner padding of 1rem
     shellOutputResult.style.height = `${shellOutput.clientHeight - 2 * rem}px`;
     shellOutputResult.style.width = `${shellOutput.clientWidth - 2 * rem}px`;
   }
+
+  console.log(`REM: ${rem}`);
+  console.log(`Code Editor: ${codeEditor.clientWidth}x${codeEditor.clientHeight}`);
+  console.log(`Code Editor Textarea: ${codeTextArea.clientWidth}x${codeTextArea.clientHeight}`);
+  console.log(`Code Editor Highlight Field: ${codeTextAreaHighlightField.clientWidth}x${codeTextAreaHighlightField.clientHeight}`);
+  console.log(`Shell Output: ${shellOutput.clientWidth}x${shellOutput.clientHeight}`);
+  console.log(`Shell Output Result: ${shellOutputResult.clientWidth}x${shellOutputResult.clientHeight}`);
 }
 
 // Properly configure the sizes of the items in the browser window. This should set every item relative to the maximum
