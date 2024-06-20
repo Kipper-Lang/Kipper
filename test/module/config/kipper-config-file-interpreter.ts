273611c0ee8a2c490f0c779cbd118980a7b1f13f
Luna-Klatzer <dev@lklatzer.com>
2024-06-20 20:49:49 +0200

    other: Fixed test files for the `@kipper/config` package

--- test/module/config/kipper-config-file-interpreter.ts
+++ test/module/config/kipper-config-file-interpreter.ts
@@ -3,6 +3,7 @@ import { assert } from "chai";
 import { KipperConfigFile, KipperConfigInterpreter, version as kipConfigVersion } from "@kipper/config";
 import * as semver from "semver";
 import * as path from "node:path";
+import { KipperTypeScriptTarget } from "@kipper/target-ts";
 
 const basicKipConfig = getFileName("kip-config.basic.json");
 const extendsKipConfig = getFileName("kip-config.extended.json");
@@ -20,6 +21,7 @@ describe("KipperConfigInterpreter", () => {
 			{ file: getFileName("errors/missing-required-field.json"), error: "ConfigValidationError" },
 			{ file: getFileName("errors/invalid-type.json"), error: "ConfigValidationError" },
 			{ file: getFileName("errors/invalid-array-element-type.json"), error: "ConfigValidationError" },
+			{ file: getFileName("errors/unknown-target.json"), error: "UnknownTargetError" },
 		];
 
 		describe("no extends", () => {
@@ -28,32 +30,19 @@ describe("KipperConfigInterpreter", () => {
 				const config = await interpreter.loadConfig(kipperConfigFile);
 
 				const pwd = process.cwd();
-				assert.deepEqual(config.raw, {
-					basePath: pwd,
-					srcDir: `${pwd}/test/kipper-files`,
-					outDir: `${pwd}/dist`,
-					compiler: {
-						version: semver.parse(semver.clean(kipConfigVersion))!,
-						target: "ts",
-					},
-					files: [{ src: `${pwd}/test/kipper-files/main.kip`, outDir: `${pwd}/dist` }],
-					resources: [
-						{ src: `${pwd}/img/icon.png`, out: `${pwd}/dist/img/icon.png` },
-						{ src: `${pwd}/img/Kipper-Logo-without-head.png`, out: `${pwd}/dist/new-img-folder/icon.png` },
-					],
-				});
-			});
-
-			errorTestCases.forEach(({ file, error }) => {
-				it(`should throw '${error}' if the file is '${path.basename(file)}'`, async () => {
-					try {
-						const kipperConfigFile = await KipperConfigFile.fromFile(file, "utf8");
-						await interpreter.loadConfig(kipperConfigFile);
-						assert.fail("Expected an error to be thrown");
-					} catch (err) {
-						assert.equal((<Error>err).constructor.name, error);
-					}
-				});
+				assert.equal(config.basePath, pwd);
+				assert.equal(config.srcDir, `${pwd}/test/kipper-files`);
+				assert.equal(config.outDir, `${pwd}/dist`);
+				assert.deepEqual(config.compiler.version, semver.parse(semver.clean(kipConfigVersion)));
+				assert.instanceOf(config.compiler.target, KipperTypeScriptTarget);
+				assert.deepEqual(config.files, [{
+					src: `${pwd}/test/kipper-files/main.kip`,
+					outDir: `${pwd}/dist`
+				}]);
+				assert.deepEqual(config.resources, [
+					{ src: `${pwd}/img/icon.png`, out: `${pwd}/dist/img/icon.png` },
+					{ src: `${pwd}/img/Kipper-Logo-without-head.png`, out: `${pwd}/dist/new-img-folder/icon.png` },
+				]);
 			});
 		});
 
@@ -63,28 +52,26 @@ describe("KipperConfigInterpreter", () => {
 				const config = await interpreter.loadConfig(kipperConfigFile);
 
 				const pwd = process.cwd();
-				assert.deepEqual(config.raw, {
-					basePath: pwd,
-					srcDir: `${pwd}/test/kipper-files`,
-					outDir: `${pwd}/dist`,
-					compiler: {
-						version: semver.parse(semver.clean(kipConfigVersion))!,
-						target: "ts",
-					},
-					files: [
-						{ src: `${pwd}/test/kipper-files/multi-function-definition.kip`, outDir: `${pwd}/dist` },
-						{ src: `${pwd}/test/kipper-files/multi-function-call.kip`, outDir: `${pwd}/dist` },
-						{ src: `${pwd}/test/kipper-files/main.kip`, outDir: `${pwd}/dist` },
-					],
-					resources: [
-						{ src: `${pwd}/img/Kipper-Logo-with-head.png`, out: `${pwd}/dist/img/Kipper-Logo-with-head.png` },
-						{ src: `${pwd}/img/Kipper-Icon.png`, out: `${pwd}/dist/img/Kipper-Icon.png` },
-						{ src: `${pwd}/img/icon.png`, out: `${pwd}/dist/img/icon.png` },
-						{ src: `${pwd}/img/Kipper-Logo-without-head.png`, out: `${pwd}/dist/new-img-folder/icon.png` },
-					],
-				});
+				assert.equal(config.basePath, pwd);
+				assert.equal(config.srcDir, `${pwd}/test/kipper-files`);
+				assert.equal(config.outDir, `${pwd}/dist`);
+				assert.deepEqual(config.compiler.version, semver.parse(semver.clean(kipConfigVersion)));
+				assert.instanceOf(config.compiler.target, KipperTypeScriptTarget);
+				assert.deepEqual(config.files, [
+					{ src: `${pwd}/test/kipper-files/multi-function-definition.kip`, outDir: `${pwd}/dist` },
+					{ src: `${pwd}/test/kipper-files/multi-function-call.kip`, outDir: `${pwd}/dist` },
+					{ src: `${pwd}/test/kipper-files/main.kip`, outDir: `${pwd}/dist` },
+				]);
+				assert.deepEqual(config.resources, [
+					{ src: `${pwd}/img/Kipper-Logo-with-head.png`, out: `${pwd}/dist/img/Kipper-Logo-with-head.png` },
+					{ src: `${pwd}/img/Kipper-Icon.png`, out: `${pwd}/dist/img/Kipper-Icon.png` },
+					{ src: `${pwd}/img/icon.png`, out: `${pwd}/dist/img/icon.png` },
+					{ src: `${pwd}/img/Kipper-Logo-without-head.png`, out: `${pwd}/dist/new-img-folder/icon.png` },
+				]);
 			});
+		});
 
+		describe("errors", () => {
 			errorTestCases.forEach(({ file, error }) => {
 				it(`should throw '${error}' if the file is '${path.basename(file)}'`, async () => {
 					try {
