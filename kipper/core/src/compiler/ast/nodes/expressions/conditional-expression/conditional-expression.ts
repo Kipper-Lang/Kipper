f40897f1d6836cdaaf9c2ec23f4a2bf24557ac11
Fabian <peacemakerdude3@gmail.com>
2024-07-02 11:54:08 +0200

    minor (534) Finished AST Node for Ternary Expression

--- kipper/core/src/compiler/ast/nodes/expressions/conditional-expression/conditional-expression.ts
+++ kipper/core/src/compiler/ast/nodes/expressions/conditional-expression/conditional-expression.ts
@@ -13,6 +13,7 @@ import { Expression } from "../expression";
 import type { ConditionalExpressionContext } from "../../../../parser";
 import { KindParseRuleMapping, ParseRuleKindMapping } from "../../../../parser";
 import { KipperNotImplementedError } from "../../../../../errors";
+import * as console from "node:console";
 
 /**
  * Conditional expression, which evaluates a condition and evaluates the left expression if it is true, or the right
@@ -82,9 +83,19 @@ export class ConditionalExpression extends Expression<
 	 * the children has already failed and as such no parent node should run type checking.
 	 */
 	public async primarySemanticAnalysis(): Promise<void> {
-		throw this.programCtx
-			.semanticCheck(this)
-			.notImplementedError(new KipperNotImplementedError("Conditional Expressions have not been implemented yet."));
+		const condition = this.children[0];
+		const trueBranch = this.children[1];
+		const falseBranch = this.children[2];
+
+		if (!condition || !trueBranch || !falseBranch) {
+			throw new KipperNotImplementedError("ConditionalExpression.primarySemanticAnalysis");
+		}
+
+		this.semanticData = {
+			condition: condition,
+			trueBranch: trueBranch,
+			falseBranch: falseBranch,
+		};
 	}
 
 	/**
@@ -96,9 +107,12 @@ export class ConditionalExpression extends Expression<
 	 * @since 0.7.0
 	 */
 	public async primarySemanticTypeChecking(): Promise<void> {
-		throw this.programCtx
-			.semanticCheck(this)
-			.notImplementedError(new KipperNotImplementedError("Conditional Expressions have not been implemented yet."));
+		const semanticData = this.getSemanticData();
+		this.programCtx.typeCheck(this).validConditionalExpression(semanticData.trueBranch, semanticData.falseBranch);
+
+		this.typeSemantics = {
+			evaluatedType: semanticData.trueBranch.getTypeSemanticData().evaluatedType,
+		};
 	}
 
 	/**
