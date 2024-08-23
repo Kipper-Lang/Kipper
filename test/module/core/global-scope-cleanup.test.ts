90788ae2d56f99cad528c55d4a5fcee0f4e36a02
Luna-Klatzer <dev@lklatzer.com>
2024-08-23 18:24:18 +0200

    other: Fixed global scope pollution issues in `@kipper/core` tests

---
+++ test/module/core/global-scope-cleanup.test.ts
@@ -0,0 +1,6 @@
+afterEach(() => {
+	// @ts-ignore
+	var __globalScope = typeof __globalScope !== "undefined" ? __globalScope : typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
+	// @ts-ignore
+	__globalScope.__kipper = undefined;
+});
