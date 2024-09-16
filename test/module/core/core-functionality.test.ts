3d1a9509a61d498bd3dbcbfc2c8c223ff168c522
Luna-Klatzer <luna.klatzer@gmail.com>
2024-09-16 19:05:10 +0200

    other: Added matches expression tests

--- test/module/core/core-functionality.test.ts
+++ test/module/core/core-functionality.test.ts
@@ -1668,6 +1668,92 @@ describe("Core functionality", () => {
 		});
 	});
 
+	describe("Matches", () => {
+		it("should return true for an empty interface with an empty object", async () => {
+			const fileContent = `interface Test { }; var x: obj = { }; print(x matches Test);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "true", "Expected different output"), jsCode);
+		});
+
+		it("should return true for an empty interface with an object with properties", async () => {
+			const fileContent = `interface Test { }; var x: obj = { a: 1, b: '2' }; print(x matches Test);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "true", "Expected different output"), jsCode);
+		});
+
+		it("should return false for an interface with properties with an object with less properties", async () => {
+			const fileContent = `interface Test { a: num; b: str; }; var x: obj = { a: 1 }; print(x matches Test);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "false", "Expected different output"), jsCode);
+		});
+
+		it("should return true for an interface with properties with an object with more properties", async () => {
+			const fileContent = `interface Test { a: num; b: str; }; var x: obj = { a: 1, b: '2', c: true }; print(x matches Test);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "true", "Expected different output"), jsCode);
+		});
+
+		it("should return false for an interface with properties with an object with different properties", async () => {
+			const fileContent = `interface Test { a: num; b: str; }; var x: obj = { a: 1, c: '2' }; print(x matches Test);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "false", "Expected different output"), jsCode);
+		});
+
+		it("should support generics such as arrays and lambda", async () => {
+			const fileContent = `interface Test { a: Array<num>; b: Func<num, num>; }; var x: obj = { a: [1, 2, 3], b: (x: num): num -> x + 1 }; print(x matches Test);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "true", "Expected different output"), jsCode);
+		});
+
+		it("should support nested interfaces", async () => {
+			const fileContent = `interface Test { a: num; b: str; }; interface Test2 { c: Test; }; var x: obj = { c: { a: 1, b: '2' } }; print(x matches Test2);`;
+			const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+			assert.isDefined(instance.programCtx);
+			assert.equal(instance.programCtx!!.errors.length, 0, "Expected no compilation errors");
+
+			const written = instance.write();
+			const jsCode = ts.transpile(written);
+			testPrintOutput((message: any) => assert.equal(message, "true", "Expected different output"), jsCode);
+		});
+	});
+
 	describe("Instanceof", () => {
 		it("should return true when object is instance of class", async () => {
 			const fileContent = `class Test {a: str; constructor (b: str) {this.a = b;}}; var x: Test = new Test("3"); print(x instanceof Test);`;
