8cc2406380a11451c2d8195212098cb32bfe92f4
Luna-Klatzer <dev@lklatzer.com>
2025-09-19 10:04:00 +0200

    other: Adjusted tests to reflect changes in 0d028ee954bd49aadce98bd369a59a2e65a7a85b

--- test/module/core/core-functionality/try-catch.test.ts
+++ test/module/core/core-functionality/try-catch.test.ts
@@ -15,9 +15,15 @@ describe("Try-Catch statements", () => {
 		const written = instance.write();
 		assert.include(
 			written,
-			`try\n{\n  x = 5;\n}\ncatch (__e_1: unknown) {\n  if (__e_1 instanceof CustomError)\n  {\n    x = 6;\n  }\n}\n__kipper.print(x);`,
+			`try\n{\n  x = 5;\n}\ncatch (__kipper_e)`,
 			"Invalid TypeScript code (Expected different output)",
 		);
+		assert.include(
+			written,
+			`if (__kipper_e instanceof CustomError) {\n    const e = __kipper_e;\n    x = 6;\n  }`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(written, `throw __kipper_e;`, "Invalid TypeScript code (Expected different output)");
 
 		const jsCode = ts.transpile(instance.write(), { target: ScriptTarget.ES2015 });
 		testPrintOutput((message: any) => assert.equal(message, "5", "Expected different output"), jsCode);
@@ -40,29 +46,109 @@ describe("Try-Catch statements", () => {
 		const written = instance.write();
 		assert.include(
 			written,
-			`try\n{\n  x = 5;\n}\ncatch (__e_1: unknown) {\n  if (__e_1 instanceof CustomError1)\n  {\n    x = 6;\n  }\n  if (__e_1 instanceof CustomError2)\n  {\n    x = 7;\n  }\n}\n__kipper.print(x);`,
+			`try\n{\n  x = 5;\n}\ncatch (__kipper_e)`,
 			"Invalid TypeScript code (Expected different output)",
 		);
+		assert.include(
+			written,
+			`if (__kipper_e instanceof CustomError1) {\n    const e = __kipper_e;\n    x = 6;\n  }`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(
+			written,
+			`if (__kipper_e instanceof CustomError2) {\n    const e = __kipper_e;\n    x = 7;\n  }`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(written, `throw __kipper_e;`, "Invalid TypeScript code (Expected different output)");
 
 		const jsCode = ts.transpile(instance.write(), { target: ScriptTarget.ES2015 });
 		testPrintOutput((message: any) => assert.equal(message, "5", "Expected different output"), jsCode);
 	});
 
-	it("should be able to catch errors using try-empty-catch", async () => {
-		const fileContent = `var x: num = 4; try { x = 5; } catch ( ) { x = 6; } print(x);`;
+	it("should allow nested try-catch statement in try block", async () => {
+		const fileContent = `
+			class CustomErrorA {}
+			class CustomErrorB {}
+			var x: num = 1;
+			try {
+				try {
+					x = 2;
+				} catch (e: CustomErrorA) {
+					x = 3;
+				}
+			} catch (e: CustomErrorB) {
+				x = 4;
+			}
+			print(x);
+  	`;
 		const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
 
 		assert.isDefined(instance.programCtx);
 		assert.deepEqual(instance.programCtx!!.errors, [], "Expected no compilation errors");
 		const written = instance.write();
+
 		assert.include(
 			written,
-			`try\n{\n  x = 5;\n}\ncatch (__e_1: unknown)\n{\n  x = 6;\n}\n__kipper.print(x);`,
+			`try\n{\n  try\n  {\n    x = 2;\n  }\n  catch (__kipper_e)`,
 			"Invalid TypeScript code (Expected different output)",
 		);
+		assert.include(
+			written,
+			`if (__kipper_e instanceof CustomErrorA) {\n      const e = __kipper_e;\n      x = 3;\n    }`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(
+			written,
+			`catch (__kipper_e)\n{\n  if (__kipper_e instanceof CustomErrorB) {\n    const e = __kipper_e;\n    x = 4;\n  }`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(written, `throw __kipper_e;`, "Invalid TypeScript code (Expected different output)");
 
 		const jsCode = ts.transpile(instance.write(), { target: ScriptTarget.ES2015 });
-		testPrintOutput((message: any) => assert.equal(message, "5", "Expected different output"), jsCode);
+		testPrintOutput((message: any) => assert.equal(message, "2", "Expected different output"), jsCode);
+	});
+
+	it("should allow nested try-catch statement in catch block", async () => {
+		const fileContent = `
+			class CustomErrorA {}
+			class CustomErrorB {}
+			var x: num = 1;
+			try {
+				x = 2;
+			} catch (e: CustomErrorA) {
+				try {
+					x = 3;
+				} catch (e: CustomErrorB) {
+					x = 4;
+				}
+			}
+			print(x);
+		`;
+		const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+		assert.isDefined(instance.programCtx);
+		assert.deepEqual(instance.programCtx!!.errors, [], "Expected no compilation errors");
+		const written = instance.write();
+
+		assert.include(
+			written,
+			`try\n{\n  x = 2;\n}\ncatch (__kipper_e)`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(
+			written,
+			`if (__kipper_e instanceof CustomErrorA) {\n    const e = __kipper_e;\n    try\n    {\n      x = 3;\n    }\n    catch (__kipper_e)`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(
+			written,
+			`if (__kipper_e instanceof CustomErrorB) {\n        const e = __kipper_e;\n        x = 4;\n      }`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+		assert.include(written, `throw __kipper_e;`, "Invalid TypeScript code (Expected different output)");
+
+		const jsCode = ts.transpile(instance.write(), { target: ScriptTarget.ES2015 });
+		testPrintOutput((message: any) => assert.equal(message, "2", "Expected different output"), jsCode);
 	});
 
 	it("should be able to catch errors using try-finally", async () => {
@@ -72,9 +158,10 @@ describe("Try-Catch statements", () => {
 		assert.isDefined(instance.programCtx);
 		assert.deepEqual(instance.programCtx!!.errors, [], "Expected no compilation errors");
 		const written = instance.write();
+
 		assert.include(
 			written,
-			`try\n{\n  x = 5;\n}\nfinally\n{\n  x = 7;\n}\n__kipper.print(x);`,
+			`try\n{\n  x = 5;\n}\nfinally\n{\n  x = 7;\n}`,
 			"Invalid TypeScript code (Expected different output)",
 		);
 
