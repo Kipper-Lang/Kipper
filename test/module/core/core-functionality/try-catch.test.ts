faf1cb727f52b730a4476498f1f33796c2de4169
Luna-Klatzer <dev@lklatzer.com>
2025-09-25 09:23:01 +0200

    other: Added tests for `InvalidErrorBindingTypeError` and catch-all catch blocks

--- test/module/core/core-functionality/try-catch.test.ts
+++ test/module/core/core-functionality/try-catch.test.ts
@@ -6,6 +6,42 @@ import { ScriptTarget } from "typescript";
 import { testPrintOutput } from "../index";
 
 describe("Try-Catch Statements", () => {
+	it("should be able to catch all errors using default catch", async () => {
+		const fileContent = `var x: num = 4; try { x = 5; } catch (_) { x = 6; } print(x);`;
+		const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+		assert.isDefined(instance.programCtx);
+		assert.deepEqual(instance.programCtx!!.errors, [], "Expected no compilation errors");
+
+		const written = instance.write();
+		assert.include(
+			written,
+			`try\n{\n  x = 5;\n}\ncatch (__kipper_e)\n{\n  if (true) {\n    const _ = __kipper_e;\n    x = 6;\n  }\n}`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+
+		const jsCode = ts.transpile(instance.write(), { target: ScriptTarget.ES2015 });
+		testPrintOutput((message: any) => assert.equal(message, "5", "Expected different output"), jsCode);
+	});
+
+	it("should be able to catch all errors using any catch", async () => {
+		const fileContent = `var x: num = 4; try { x = 5; } catch (e: any) { x = 6; } print(x);`;
+		const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
+
+		assert.isDefined(instance.programCtx);
+		assert.deepEqual(instance.programCtx!!.errors, [], "Expected no compilation errors");
+
+		const written = instance.write();
+		assert.include(
+			written,
+			`try\n{\n  x = 5;\n}\ncatch (__kipper_e)\n{\n  if (true) {\n    const e = __kipper_e;\n    x = 6;\n  }\n}`,
+			"Invalid TypeScript code (Expected different output)",
+		);
+
+		const jsCode = ts.transpile(instance.write(), { target: ScriptTarget.ES2015 });
+		testPrintOutput((message: any) => assert.equal(message, "5", "Expected different output"), jsCode);
+	});
+
 	it("should be able to catch errors using try-catch", async () => {
 		const fileContent = `class CustomError {} var x: num = 4; try { x = 5; } catch (e: CustomError) { x = 6; } print(x);`;
 		const instance: KipperCompileResult = await compiler.compile(fileContent, { target: defaultTarget });
